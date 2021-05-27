a29512a93985c041305aabf5b2839046b749dc17
thaknal <krzysiekpnq50@gmail.com>
2021-05-28 00:26:46 +0200

    Zadanie 6

--- src/pl/lublin/wsei/java/cwiczenia/MyDB.java
+++ src/pl/lublin/wsei/java/cwiczenia/MyDB.java
@@ -1,8 +1,6 @@
 package pl.lublin.wsei.java.cwiczenia;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.SQLException;
+import java.sql.*;
 import java.util.Properties;
 
 public class MyDB {
@@ -11,6 +9,9 @@ public class MyDB {
     private String dbName;
     private String user;
     private String password;
+
+    private Connection conn = null;
+    private Statement statement = null;
 
     public MyDB(String host, Number port, String dbName) {
         this.host = host;
@@ -34,41 +35,50 @@ public class MyDB {
         this.password = password;
     }
 
-    private Connection conn = null;
-
-    private void connect() {
+    private void connect(){
         Properties connectionProps = new Properties();
         connectionProps.put("user", user);
         connectionProps.put("password", password);
-        connectionProps.put("serverTimezone", "Europe/Warsaw");
+        connectionProps.put("serverTimeZone", "Europe/Warsaw");
 
         String jdbcString = "jdbc:mysql://" + host + ":" + port + "/" + dbName;
         try {
-            conn = DriverManager.getConnection(jdbcString, connectionProps);
-        }
+            conn = DriverManager.getConnection(
+                    jdbcString, connectionProps);
+            statement = conn.createStatement();
+        } catch (SQLException e){
+            System.out.println("Błąd podłączenia do bazy " + dbName);
+            System.out.println("Komunikat błędu: " + e.getMessage());
 
-        catch (SQLException e) {
-            System.out.println("Błąd podłączenia do bazy: " + jdbcString);
-            System.out.println("Komunikat błędu: " +e.getMessage());
             conn = null;
         }
-
-        System.out.println("Connected do database " + dbName);
-
+        System.out.println("Connected to database");
     }
-    public Connection getConnection() {
-        if (conn == null)
-            connect();
+
+    public Connection getConnection(){
+        if(conn == null) connect();
         return conn;
     }
-    public void closeConnection(){
-        if (conn != null)
-            try {
-                conn.close();
 
+    public void closeConnection(){
+        if(conn != null){
+            try{
+                conn.close();
             } catch (SQLException e) {
-                System.out.println("Błąd przy zamykaniu połączenia bazodanowego" + e.getMessage());
+                System.out.println("Błąd przy zamykaniu połączenia bazdanowego: " + e.getMessage());
             }
+        }
         conn = null;
+    }
+
+    public ResultSet selectData(String selectStatement){
+        if((conn != null) && (statement != null)) {
+            try {
+                return statement.executeQuery(selectStatement);
+            } catch (SQLException e) {
+                System.out.println("Błąd realizacji zapytania " + selectStatement + ": " + e.getMessage());
+            }
+        }
+        return null;
     }
 }
